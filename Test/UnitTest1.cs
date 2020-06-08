e1aa4d05a22172c2015a3810a247b8d06e878741
Діма Калинюк <dima.kalinyuk.2019@ukr.net>
2020-06-09 01:06:55 +0300

    Laba5.3

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double[] b = {29, 28, 81, 74, 71};
+            int result = (int)DimaOOP5.Marsh.Number();
+            Assert.AreEqual(283, result);
+
+        }
+    }
+}
